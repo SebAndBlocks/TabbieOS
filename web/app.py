bfa402ac2df0d3b5913d36f474560e079aea2f8e
Seb <sebastianneill2011@gmail.com>
2023-11-03 22:00:41 +0000

    updates

--- web/app.py
+++ web/app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import json as jx
 
 app = Flask(__name__)
+
+def isMasterPassword(pswd):
+    save = open("save.json")
+    if (jx.loads(save.read)["master_pswd"] == pswd){
+        save.close()
+        return True
+    } else {
+        save.close()
+        return False
+    }
+
+def wondeData(pswd):
+    if (isMasterPassword(pswd) = False) {
+        return "ERROR1"
+    }
+    
+
 
 @app.route("/")
 def hello_world():
