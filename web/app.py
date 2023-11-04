f7723080d87c398e33c8a4c4b4dd838ac0e11b02
Seb <sebastianneill2011@gmail.com>
2023-11-04 08:14:05 +0000

    building HTML

--- web/app.py
+++ web/app.py
@@ -5,21 +5,13 @@ app = Flask(__name__)
 
 def isMasterPassword(pswd):
     save = open("save.json")
-    if (jx.loads(save.read)["master_pswd"] == pswd){
+    if (jx.loads(save.read)["master_pswd"] == pswd):
         save.close()
         return True
-    } else {
+    else :
         save.close()
         return False
-    }
-
-def wondeData(pswd):
-    if (isMasterPassword(pswd) = False) {
-        return "ERROR1"
-    }
     
-
-
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
