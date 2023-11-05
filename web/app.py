2dfdedff8a653879efeaad37d5aa1c013ae125ce
Seb <sebtnt@proton.me>
2023-11-05 19:15:17 +0000

    yay

--- web/app.py
+++ web/app.py
@@ -1,25 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 import json as jx
 
 app = Flask(__name__)
+ver = "0.1"
 
 def getHomePageSave():
-    sraw = open("save.json")
-    sjson = jx.loads(sraw.read())
-    return sjson["home_page"]
+    try:
+        sraw = open("save.json")
+        sjson = jx.loads(sraw.read())
+        return sjson["home_page"]
+    except FileNotFoundError:
+        return FileNotFoundError
 
 def isMasterPassword(pswd):
-    save = open("save.json")
-    if (jx.loads(save.read)["master_pswd"] == pswd):
-        save.close()
-        return True
-    else :
-        save.close()
-        return False
+    try:
+        save = open("save.json")
+        if (jx.loads(save.read)["master_pswd"] == pswd):
+            save.close()
+            return True
+        else :
+            save.close()
+            return False
+    except FileNotFoundError:
+        return "nosave"
     
 @app.route("/")
-def hello_world():
-    print(getHomePageSave())
-    print(getHomePageSave()["welcometext"])
-    print(getHomePageSave()["pagetitle"])
+def home():
     return render_template("home.html", wt=getHomePageSave()["welcometext"], name=getHomePageSave()["pagetitle"])
